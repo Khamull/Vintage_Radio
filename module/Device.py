16e0e11f07a513594e32c698cd21b2a544898bfb
Charles Path <charles.path@outlook.com>
2021-01-25 03:10:13 -0300

    Small fix
    serial = spi(device=0, port=0, cs_high=True)#cs_high = True is a workaround to this isseu https://github.com/raspberrypi/linux/issues/3355
    This new option fixes an issues that was introduced in a update of the system

--- module/Device.py
+++ module/Device.py
@@ -15,7 +15,8 @@ import os
 def get_device():
     # rev.1 users set port=0
     # substitute spi(device=0, port=0) below if using that interface
-    serial = spi(device=0, port=0)
+    serial = spi(device=0, port=0, cs_high=True)#cs_high = True is a workaround to this isseu https://github.com/raspberrypi/linux/issues/3355
+    #s erial = spi(device=0, port=0)
     # substitute ssd1331(...) or sh1106(...) below if using that device
     device = ssd1309(serial)
     return device
